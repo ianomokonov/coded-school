40b09b2cb96bd635943247bb512243895560f836
Ivan Nomokonov <inomokonov@it-one.ru>
2024-02-06 16:02:57 +0300

    CODED-33 поправил тест на переход на страницу входа

--- client/src/app/core/jwt/interceptor.spec.ts
+++ client/src/app/core/jwt/interceptor.spec.ts
@@ -5,26 +5,21 @@ import { TestBed } from '@angular/core/testing';
 import { JwtService } from '@jwt/service';
 import { refreshedTokensMock, tokensMock } from '../../secure/_tests-mocks/tokens.mock';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { of, throwError } from 'rxjs';
 
 describe('TokenInterceptor', () => {
     let httpTestingController: HttpTestingController;
     let httpClient: HttpClient;
-    let fakeTokenService: jasmine.SpyObj<JwtService>;
+    const fakeTokenService: JwtService = {
+        getRefreshToken: () => tokensMock.refreshToken,
+        getToken: () => tokensMock.token,
+        storeTokens: () => {},
+        removeTokens: () => {},
+        refreshToken: () => of(refreshedTokensMock),
+    } as any;
     let router: Router;
-    let isRefreshError = false;
 
     beforeEach(() => {
-        fakeTokenService = jasmine.createSpyObj('JwtService', {
-            getRefreshToken: tokensMock.refreshToken,
-            getToken: tokensMock.token,
-            storeTokens: () => {},
-            removeTokens: () => {},
-            refreshToken: new Observable((observer) => {
-                isRefreshError ? observer.error('error123') : observer.next(refreshedTokensMock);
-                observer.complete();
-            }),
-        });
         TestBed.configureTestingModule({
             providers: [
                 provideHttpClient(withInterceptors([TokenInterceptor])),
@@ -35,10 +30,6 @@ describe('TokenInterceptor', () => {
         httpTestingController = TestBed.inject(HttpTestingController);
         httpClient = TestBed.inject(HttpClient);
         router = TestBed.inject(Router);
-    });
-
-    afterEach(() => {
-        httpTestingController.verify();
     });
 
     it('should intercept /api requests', () => {
@@ -53,13 +44,15 @@ describe('TokenInterceptor', () => {
 
     it('should refresh tokens if access token expired', () => {
         const url = '/api/user';
-        isRefreshError = false;
+        const refreshSpy = spyOn(fakeTokenService, 'refreshToken').and.returnValue(
+            of(refreshedTokensMock),
+        );
 
         httpClient.get(url).subscribe();
 
         httpTestingController.expectOne(url).error(new ProgressEvent('error'), { status: 401 });
 
-        expect(fakeTokenService.refreshToken).toHaveBeenCalledTimes(1);
+        expect(refreshSpy).toHaveBeenCalledTimes(1);
 
         const req = httpTestingController.expectOne(url);
 
@@ -68,19 +61,16 @@ describe('TokenInterceptor', () => {
         );
     });
 
-    // it('should redirect to login form if refresh token expired', () => {
-    //     const url = '/api/user';
-    //     isRefreshError = true;
-    //
-    //     httpClient.get(url).subscribe();
-    //
-    //     httpTestingController.expectOne(url).error(new ProgressEvent('error'), { status: 401 });
-    //
-    //     expect(fakeTokenService.refreshToken).toThrowError('error123');
-    //
-    //     const navigateSpy = spyOn(router, 'navigate').and.returnValue({} as any);
-    //
-    //     expect(navigateSpy).toHaveBeenCalledTimes(1);
-    //     expect(navigateSpy).toHaveBeenCalledWith(['/sign-in']);
-    // });
+    it('should redirect to login form if refresh token expired', () => {
+        const url = '/api/user';
+        spyOn(fakeTokenService, 'refreshToken').and.returnValue(throwError(() => 'error123'));
+        const navigateSpy = spyOn(router, 'navigate').and.returnValue({} as any);
+
+        httpClient.get(url).subscribe({ error: () => {} });
+
+        httpTestingController.expectOne(url).error(new ProgressEvent('error'), { status: 401 });
+
+        expect(navigateSpy).toHaveBeenCalledTimes(1);
+        expect(navigateSpy).toHaveBeenCalledWith(['/sign-in']);
+    });
 });
