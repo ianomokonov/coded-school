db58fc7e41963431eac28d4069d8f98626366539
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2024-02-07 12:04:35 +0300

    починил миграцию заполнения ролей

--- server/migrations/1707231989655-roles.ts
+++ server/migrations/1707231989655-roles.ts
@@ -2,8 +2,14 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Roles1707231989655 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE IF NOT EXISTS role (
+      id INT PRIMARY KEY AUTO_INCREMENT,
+      name VARCHAR(255)
+    )`);
     queryRunner.query(`INSERT INTO role (name) VALUES ('user'), ('admin')`);
   }
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`DELETE FROM role WHERE name in ('user', 'admin')`);
+  }
 }
